aa57beaa46e8fe35991f51d09bc9beb170b61c19
Victor Vargas <v.conejo@live.com>
2019-03-09 17:23:55 -0700

    readded more stateents, now trying to get range

--- vvargas_prog3.c
+++ vvargas_prog3.c
@@ -47,11 +47,9 @@ int main(int argc, char *argv[])
 
   /*Now we need to compute the missing high and low, to achieve a good balance*/
   //FIXME: We will leave it at 50/50 for now
-  /*
   int range = primeThreadData[1].high-primeThreadData[0].low;
   primeThreadData[0].high = range/2;
   primeThreadData[1].low = range/2+1;
-  */
   printf("Did not create threads");
   /*Create threads that will do the prime search*/
  /* pthread_create(&tid[0],&attr,prime_search,&primeThreadData[0]);//thread 1
