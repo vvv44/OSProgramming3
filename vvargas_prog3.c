6e822426220b93619cd6813233434868dc31afff
Victor Vargas <v.conejo@live.com>
2019-03-09 17:24:43 -0700

    now trying to create threads

--- vvargas_prog3.c
+++ vvargas_prog3.c
@@ -52,9 +52,9 @@ int main(int argc, char *argv[])
   primeThreadData[1].low = range/2+1;
   printf("Did not create threads");
   /*Create threads that will do the prime search*/
- /* pthread_create(&tid[0],&attr,prime_search,&primeThreadData[0]);//thread 1
+  pthread_create(&tid[0],&attr,prime_search,&primeThreadData[0]);//thread 1
   pthread_create(&tid[1],&attr,prime_search,&primeThreadData[1]);//thread 2
-*/
+
   printf("What is going on guys");
   /* Setup a mini shell thread to provide interactivity with the user */
   pthread_create(&tidshell,&attr,mini_shell,NULL);
